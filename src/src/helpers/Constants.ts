c5f24d3186e14c2b9ef6a3415d295bad6104c8fb
Karl Yann Fouezeu Zeufack <karlydiamond214@gmail.com>
2021-06-08 00:56:23 +0200

    oidc settings changed

--- src/src/helpers/Constants.ts
+++ src/src/helpers/Constants.ts
@@ -1,8 +1,8 @@
 export class Constants {
-  public static stsAuthority = 'https://demo.identityserver.io/';
-  public static clientId = 'interactive.public';
+  public static stsAuthority = 'https://api.learning-layers.eu/o/oauth2';
+  public static clientId = 'assessment_handler';
   public static clientRoot = 'http://localhost:4200/';
-  public static clientScope = 'openid profile email api';
+  public static clientScope = 'openid profile';
 
-  public static apiRoot = 'https://demo.identityserver.io/api/';
+  public static apiRoot = 'https://api.learning-layers.eu/o/oauth2';
 }
